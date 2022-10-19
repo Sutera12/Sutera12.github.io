d6fa373d3e6781065d9131f23c44466f4dd6407e
Sutera12 <m0a211948d@edu.teu.ac.jp>
2022-10-19 18:29:52 +0900

    2022秋javascript練習

--- 221019/function-test.js
+++ 221019/function-test.js
@@ -2,16 +2,20 @@ function test(){
   console.log("こんにちは");
 }
 
-function test2(view1){
-  console.log(`仮引数1: ${view1}`)
+function test2(level1){
+  console.log(`level1: ${level1}`)
 }
 
 test2(2);
 
 function test3(view1,view2,view3){
-  console.log(`仮引数1: ${view1}`);
-  console.log(`仮引数2: ${view2}`);
-  console.log(`仮引数3: ${view3}`);
+  console.log(`view1: ${view1}`);
+  console.log(`view2: ${view2}`);
+  console.log(`view3: ${view3}`);
 }
 
 test3(2,8,10);
+
+function test6(low){
+  let 2021 * 1010 = `スコープ内の変数: ${}`
+}
