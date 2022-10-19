c68b4d569c4c93ae714c2d48d68f9b868484598b
Sutera12 <m0a211948d@edu.teu.ac.jp>
2022-10-19 18:20:06 +0900

    2022秋javascript練習

--- 221019/function-test.js
+++ 221019/function-test.js
@@ -2,10 +2,16 @@ function test(){
   console.log("こんにちは");
 }
 
-function view(仮引数1,仮引数2,仮引数3){
-  console.log(`仮引数1: ${仮引数1}`);
-  console.log(`仮引数2: ${仮引数2}`);
-  console.log(`仮引数3: ${仮引数3}`);
+function test2(view1){
+  console.log(`仮引数1: ${view1}`)
 }
 
-view(2,8,10);
+test2(2);
+
+function test3(view1,view2,view3){
+  console.log(`仮引数1: ${view1}`);
+  console.log(`仮引数2: ${view2}`);
+  console.log(`仮引数3: ${view3}`);
+}
+
+test3(2,8,10);
