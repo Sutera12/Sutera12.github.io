034918dd2fdac096f072d752a76aa390b7491953
Sutera12 <m0a211948d@edu.teu.ac.jp>
2022-10-19 18:12:44 +0900

    2022秋javascript練習

---
+++ 221019/function-test.js
@@ -0,0 +1,11 @@
+function test(){
+  console.log("こんにちは");
+}
+
+function view(仮引数1,仮引数2,仮引数3){
+  console.log(`仮引数1: ${仮引数1}`);
+  console.log(`仮引数2: ${仮引数2}`);
+  console.log(`仮引数3: ${仮引数3}`);
+}
+
+view(2,8,10);
