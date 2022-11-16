86d59b3494abdeb5c2821d7d577006539bdd7525
Sutera12 <m0a211948d@edu.teu.ac.jp>
2022-11-16 18:37:52 +0900

    2022秋javascript練習

--- 221116/tweets.js
+++ 221116/tweets.js
@@ -1,10 +1,16 @@
 let answer = document.querySelector("#answer")
 let html = "<ul>";
 
-let i = 0;
+/*let i = 0;
 while (i < tweets.length){
-  html += `<li><b>${tweets[i].name}</b> ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>`;
+  html += `<li><b>${tweets[i].name}</b>: ${tweets[i].message} <i>${tweets[i].tweetedAt}</i></li>`;
   i++;
+}*/
+
+let renderTweet = function(tweet){
+  html = `<li><b>${tweet.name}</b>: ${tweet.message} <i>${tweet.tweetedAt}</i></li>`;
 }
+tweets.forEach(renderTweet);
+
 html += "</ul>";
 answer.innerHTML = html;
