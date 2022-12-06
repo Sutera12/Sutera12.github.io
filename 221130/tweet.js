4562e5e62b72642f290dd64ce428956c86c1b7e4
Sutera12 <m0a211948d@edu.teu.ac.jp>
2022-12-06 23:54:21 +0900

    2022秋javascript練習

--- 221130/tweet.js
+++ 221130/tweet.js
@@ -1,5 +1,12 @@
+let tweet = document.querySelector("#tweets");
+for(let i = 0; i < tweets.length; i++){
+  tweet.innerHTML += "<p>" + tweets[i].name + "</p>";
+  tweet.innerHTML += "<p>" + tweets[i].message + "</p>";
+  tweet.innerHTML += "<p>" + tweets[i].tweetedAt + "</p>";
+  tweet.innerHTML +=`<img id='icon' style='width:60px; height: 80px' src="${tweets[i].avatar}">`;}
+
 function tweetAll(){
-  for(let i = 0; i < tweets.length; i++)
+
 };
 
 function tweetTaro(){
