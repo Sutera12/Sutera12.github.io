afd24ad828e1bc32b6685538c32626dfc3dc36e8
Sutera12 <m0a211948d@edu.teu.ac.jp>
2022-12-07 18:36:47 +0900

    2022秋javascriptの練習

---
+++ 221207/animation.js
@@ -0,0 +1,22 @@
+function startAnimation() {
+  anime({
+    targets: "#animation-box",
+    translateX: {
+      value:250,
+      duration:800
+    },
+    rotate:{
+      value:360,
+      duration: 1800,
+      easing:'easeInOutSine'
+    },
+    scale:{
+      value:2.3,
+      duration:1600,
+      delay:800,
+      easing: 'easeInOutQuart'
+    },
+    delay:250,
+  translateY:100
+  });
+}
