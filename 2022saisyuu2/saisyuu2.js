7d7a1ff60100667a56f0587fb2610b69149f13bb
Sutera12 <m0a211948d@edu.teu.ac.jp>
2023-01-18 17:20:45 +0900

    2022秋javascriptの練習

--- 2022saisyuu2/saisyuu2.js
+++ 2022saisyuu2/saisyuu2.js
@@ -39,15 +39,12 @@ if(localStorage.getItem('dark-mode-settings')==='dark') {
 
 const pagetop_btn = document.querySelector(".pagetop");
 
-// .pagetopをクリックしたら
 pagetop_btn.addEventListener("click", scroll_top);
 
-// ページ上部へスムーズに移動
 function scroll_top() {
   window.scroll({ top: 0, behavior: "smooth" });
 }
 
-// スクロールされたら表示
 window.addEventListener("scroll", scroll_event);
 function scroll_event() {
   if (window.pageYOffset > 100) {
